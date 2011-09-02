caa24c262d99842143cfc8ac73cb47238e89c52f
Lloyd Borrett <lloyd@borrett.id.au>
2011-09-03 09:29:46 +1000

    Added ShopType to photo description.

--- sql/divepics_shop.sql
+++ sql/divepics_shop.sql
@@ -1,4 +1,4 @@
-SELECT		PhotoPath as Path, ShopName as Description
+SELECT		PhotoPath as Path, CONCAT(ShopType,': ',ShopName) as Description
 FROM		$_config[table_prefix]Shop
 WHERE		ID = '$globals[id]'
 ORDER BY	ID ASC
