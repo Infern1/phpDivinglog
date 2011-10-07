f79c4505d1ba2f6a1a3c49d5b2fd4938f1586308
Lloyd Borrett <lloyd@borrett.id.au>
2011-10-08 10:33:00 +1100

    Changed sorting of results.

--- sql/citylist.sql
+++ sql/citylist.sql
@@ -2,4 +2,4 @@ SELECT		c.ID, c.City, c.Type, l.Country, COUNT(*) AS Dives
 FROM 		$_config[table_prefix]City AS c, $_config[table_prefix]Logbook AS l
 WHERE 		c.ID = l.CityID
 GROUP BY 	l.CityID
-ORDER BY 	c.City ASC
+ORDER BY 	l.Country ASC, c.City ASC
