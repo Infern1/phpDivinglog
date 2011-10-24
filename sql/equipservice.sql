7539269dfbda654b1b2d20982c8bb1b30db06b7b
Lloyd Borrett <lloyd@borrett.id.au>
2011-10-24 11:02:38 +1100

    Oops. Missed adding the PhotoPath details. Now fixed.

--- sql/equipservice.sql
+++ sql/equipservice.sql
@@ -3,7 +3,8 @@ SELECT		ID,
 		Manufacturer,
 		DateRN,
 		O2ServiceDate,
-		Inactive
+		Inactive,
+		PhotoPath
 FROM		$_config[table_prefix]Equipment
 WHERE 		((to_days( now()) > (to_days(DateRN) - $_config[equipment_service_warning]))
 OR 		(to_days( now()) > (to_days(O2ServiceDate) - $_config[equipment_service_warning])))
